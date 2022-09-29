af0cb5a7bba8b7eb06cb79973974b042dd4fbed9
s4m552 <114661742+s4m552@users.noreply.github.com>
2022-09-29 05:50:10 -0500

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,564 @@
+input.onButtonPressed(Button.A, function () {
+    // 1.LLENADO DE AGUA
+    basic.showLeds(`
+        . . # # #
+        . . # . .
+        # # # # #
+        . # # # .
+        . . # . .
+        `)
+    // 1.LLENADO DE AGUA
+    basic.showLeds(`
+        . . . # #
+        . . . . .
+        # . . . #
+        . # # # .
+        . . # . .
+        `)
+    // 1.LLENADO DE AGUA
+    basic.showLeds(`
+        . . # # #
+        . . # . .
+        # # # # #
+        . # # # .
+        . . # . .
+        `)
+    // 1.LLENADO DE AGUA
+    basic.showLeds(`
+        . . . # #
+        . . . . .
+        # . . . #
+        . # # # .
+        . . # . .
+        `)
+    // 1.LLENADO DE AGUA
+    basic.showLeds(`
+        . . # # #
+        . . # . .
+        # # # # #
+        . # # # .
+        . . # . .
+        `)
+    // 1.LLENADO DE AGUA
+    basic.showLeds(`
+        . . . # #
+        . . . . .
+        # . . . #
+        . # # # .
+        . . # . .
+        `)
+    // 1.LLENADO DE AGUA
+    basic.showLeds(`
+        . . # # #
+        . . # . .
+        # # # # #
+        . # # # .
+        . . # . .
+        `)
+    basic.pause(3000)
+    // 2.AGREGAR JABON
+    basic.showLeds(`
+        . . . . .
+        # # # # #
+        # . # . #
+        # # # # #
+        . . . . .
+        `)
+    // 2.AGREGAR JABON
+    basic.showLeds(`
+        . . . . .
+        # # # # #
+        # # # # #
+        # # # # #
+        . . . . .
+        `)
+    // 2.AGREGAR JABON
+    basic.showLeds(`
+        . . . . .
+        # # # # #
+        # . # . #
+        # # # # #
+        . . . . .
+        `)
+    // 2.AGREGAR JABON
+    basic.showLeds(`
+        . . . . .
+        # # # # #
+        # # # # #
+        # # # # #
+        . . . . .
+        `)
+    // 2.AGREGAR JABON
+    basic.showLeds(`
+        . . . . .
+        # # # # #
+        # . # . #
+        # # # # #
+        . . . . .
+        `)
+    // 2.AGREGAR JABON
+    basic.showLeds(`
+        . . . . .
+        # # # # #
+        # # # # #
+        # # # # #
+        . . . . .
+        `)
+    basic.pause(2000)
+    // 3.LAVADO
+    // 
+    basic.showLeds(`
+        . . # . .
+        . . # . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+    // 3.LAVADO
+    // 
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+    // 3.LAVADO
+    // 
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . # . .
+        . # . # .
+        . . . . .
+        `)
+    // 3.LAVADO
+    // 
+    basic.showLeds(`
+        # . . . #
+        . # . # .
+        . . # . .
+        . # . # .
+        # . . . #
+        `)
+    // 3.LAVADO
+    // 
+    basic.showLeds(`
+        . . # . .
+        . . # . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+    // 3.LAVADO
+    // 
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+    // 3.LAVADO
+    // 
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . # . .
+        . # . # .
+        . . . . .
+        `)
+    // 3.LAVADO
+    // 
+    basic.showLeds(`
+        # . . . #
+        . # . # .
+        . . # . .
+        . # . # .
+        # . . . #
+        `)
+    basic.pause(5000)
+    // 3.LAVADO
+    // 
+    basic.showLeds(`
+        . . # . .
+        . . # . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+    // 3.LAVADO
+    // 
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+    // 3.LAVADO
+    // 
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . # . .
+        . # . # .
+        . . . . .
+        `)
+    // 3.LAVADO
+    // 
+    basic.showLeds(`
+        # . . . #
+        . # . # .
+        . . # . .
+        . # . # .
+        # . . . #
+        `)
+    basic.pause(5000)
+    // 4.ESCURRIDO DE AGUA
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        # . # . #
+        . . . . .
+        . . . . .
+        `)
+    // 4.ESCURRIDO DE AGUA
+    basic.showLeds(`
+        . . . . .
+        # . # . #
+        . # . # .
+        . . . . .
+        . . . . .
+        `)
+    // 4.ESCURRIDO DE AGUA
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        # . # . #
+        . . . . .
+        . . . . .
+        `)
+    // 4.ESCURRIDO DE AGUA
+    basic.showLeds(`
+        . . . . .
+        # . # . #
+        . # . # .
+        . . . . .
+        . . . . .
+        `)
+    // 4.ESCURRIDO DE AGUA
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        # . # . #
+        . . . . .
+        . . . . .
+        `)
+    // 4.ESCURRIDO DE AGUA
+    basic.showLeds(`
+        . . . . .
+        # . # . #
+        . # . # .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(3000)
+    // 5.ENJUAGUE
+    basic.showLeds(`
+        . # . . #
+        . # . . #
+        . . # # .
+        . . . . .
+        . . . . .
+        `)
+    // 5.ENJUAGUE
+    basic.showLeds(`
+        . . . # .
+        . . . . #
+        . # . # .
+        . . # . .
+        . . . . .
+        `)
+    // 5.ENJUAGUE
+    basic.showLeds(`
+        . . # # .
+        . . . . #
+        . . . . #
+        . . # # .
+        . . . . .
+        `)
+    // 5.ENJUAGUE
+    basic.showLeds(`
+        . # . . #
+        . # . . #
+        . . # # .
+        . . . . .
+        . . . . .
+        `)
+    // 5.ENJUAGUE
+    basic.showLeds(`
+        . . . # .
+        . . . . #
+        . # . # .
+        . . # . .
+        . . . . .
+        `)
+    // 5.ENJUAGUE
+    basic.showLeds(`
+        . . # # .
+        . . . . #
+        . . . . #
+        . . # # .
+        . . . . .
+        `)
+    basic.pause(4000)
+    // 6.ESCURRIDO DE AGUA
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        # . # . #
+        . . . . .
+        . . . . .
+        `)
+    // 6.ESCURRIDO DE AGUA
+    basic.showLeds(`
+        . . . . .
+        # . # . #
+        . # . # .
+        . . . . .
+        . . . . .
+        `)
+    // 6.ESCURRIDO DE AGUA
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        # . # . #
+        . . . . .
+        . . . . .
+        `)
+    // 6.ESCURRIDO DE AGUA
+    basic.showLeds(`
+        . . . . .
+        # . # . #
+        . # . # .
+        . . . . .
+        . . . . .
+        `)
+    // 6.ESCURRIDO DE AGUA
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        # . # . #
+        . . . . .
+        . . . . .
+        `)
+    // 6.ESCURRIDO DE AGUA
+    basic.showLeds(`
+        . . . . .
+        # . # . #
+        . # . # .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(3000)
+    // 7.CENTRIFUGADO
+    basic.showLeds(`
+        . . # . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    // 7.CENTRIFUGADO
+    basic.showLeds(`
+        . # # # .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    // 7.CENTRIFUGADO
+    basic.showLeds(`
+        . # # # .
+        # . . . #
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    // 7.CENTRIFUGADO
+    basic.showLeds(`
+        . # # # .
+        # . . . #
+        # . . . #
+        . . . . .
+        . . . . .
+        `)
+    // 7.CENTRIFUGADO
+    basic.showLeds(`
+        . # # # .
+        # . . . #
+        # . . . #
+        . # . # .
+        . . . . .
+        `)
+    // 7.CENTRIFUGADO
+    basic.showLeds(`
+        . # # # .
+        # . . . #
+        # . . . #
+        . # # # .
+        . . . . .
+        `)
+    basic.pause(2000)
+    // 8.ENJUAGUE
+    basic.showLeds(`
+        . # . . #
+        . # . . #
+        . . # # .
+        . . . . .
+        . . . . .
+        `)
+    // 8.ENJUAGUE
+    basic.showLeds(`
+        . . . # .
+        . . . . #
+        . # . # .
+        . . # . .
+        . . . . .
+        `)
+    // 8.ENJUAGUE
+    basic.showLeds(`
+        . . # # .
+        . . . . #
+        . . . . #
+        . . # # .
+        . . . . .
+        `)
+    // 8.ENJUAGUE
+    basic.showLeds(`
+        . # . . #
+        . # . . #
+        . . # # .
+        . . . . .
+        . . . . .
+        `)
+    // 8.ENJUAGUE
+    basic.showLeds(`
+        . . . # .
+        . . . . #
+        . # . # .
+        . . # . .
+        . . . . .
+        `)
+    // 8.ENJUAGUE
+    basic.showLeds(`
+        . . # # .
+        . . . . #
+        . . . . #
+        . . # # .
+        . . . . .
+        `)
+    basic.pause(4000)
+    // 9.ESCURRIDO DE AGUA
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        # . # . #
+        . . . . .
+        . . . . .
+        `)
+    // 9.ESCURRIDO DE AGUA
+    basic.showLeds(`
+        . . . . .
+        # . # . #
+        . # . # .
+        . . . . .
+        . . . . .
+        `)
+    // 9.ESCURRIDO DE AGUA
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        # . # . #
+        . . . . .
+        . . . . .
+        `)
+    // 9.ESCURRIDO DE AGUA
+    basic.showLeds(`
+        . . . . .
+        # . # . #
+        . # . # .
+        . . . . .
+        . . . . .
+        `)
+    // 9.ESCURRIDO DE AGUA
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        # . # . #
+        . . . . .
+        . . . . .
+        `)
+    // 9.ESCURRIDO DE AGUA
+    basic.showLeds(`
+        . . . . .
+        # . # . #
+        . # . # .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(3000)
+    // 10.CENTRIFUGADO
+    basic.showLeds(`
+        . . # . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    // 10.CENTRIFUGADO
+    basic.showLeds(`
+        . # # # .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    // 10.CENTRIFUGADO
+    basic.showLeds(`
+        . # # # .
+        # . . . #
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    // 10.CENTRIFUGADO
+    basic.showLeds(`
+        . # # # .
+        # . . . #
+        # . . . #
+        . . . . .
+        . . . . .
+        `)
+    // 10.CENTRIFUGADO
+    basic.showLeds(`
+        . # # # .
+        # . . . #
+        # . . . #
+        . # . # .
+        . . . . .
+        `)
+    // 10.CENTRIFUGADO
+    basic.showLeds(`
+        . # # # .
+        # . . . #
+        # . . . #
+        . # # # .
+        . . . . .
+        `)
+    basic.pause(2000)
+})
+basic.forever(function () {
+	
+})
